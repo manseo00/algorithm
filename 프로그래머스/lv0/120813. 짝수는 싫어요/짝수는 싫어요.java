f90cfe4ff3933b97832806052bddf8947deebaf5
manseo00 <84060692+manseo00@users.noreply.github.com>
2023-06-17 13:44:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n) {
+        int a = 0;
+        if(n%2==0) a = n/2;
+        else a = n/2+1;
+        
+        int[] answer = new int[a];
+        int j=0;
+        for(int i=1; i<=n; i++){
+            if(i%2!=0) {
+                answer[j] = i;
+                j++;
+            }
+        }
+        
+        return answer;
+    }
+}
