fef88e0c03afd21d0cec6e39adf7b2a76495f46f
manseo00 <84060692+manseo00@users.noreply.github.com>
2023-08-25 14:37:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.85 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        int i=1;
+        while(i<=n){
+            if(i%2!=0){
+                answer += "수";
+            }else{
+                answer += "박";
+            }
+            i++;
+        }
+        
+        return answer;
+    }
+}
