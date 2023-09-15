1e0bf79618aebd9329a8f8e75edf4b25391aff9d
manseo00 <84060692+manseo00@users.noreply.github.com>
2023-09-15 15:41:06 +0900

    [unrated] Title: 수 조작하기 2, Time: 633.66 ms, Memory: 394 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+    
+        for(int i=1; i<numLog.length; i++){
+            if(numLog[i-1]-numLog[i]==1){
+                answer += "s";
+            }else if(numLog[i-1]-numLog[i]==-1){
+                answer += "w";
+            }else if(numLog[i-1]-numLog[i]==10){
+                answer += "a";
+            }else if(numLog[i-1]-numLog[i]==-10){
+                answer += "d";
+            }
+            
+            if(i==numLog.length){
+                break;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
