4ac4d02728b3695eda1f12ce15a32d594c6b4104
manseo00 <84060692+manseo00@users.noreply.github.com>
2023-09-18 18:03:35 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 11.64 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        my_string = my_string.toLowerCase();
+        char[] ch = new char[my_string.length()];
+        for(int i = 0; i < my_string.length(); i++){
+            ch[i] += my_string.charAt(i);
+        }
+        Arrays.sort(ch);
+        
+        for(int i = 0; i < ch.length; i++){
+            answer += ch[i];    
+        }
+        return answer;
+    }
+}
