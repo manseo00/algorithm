7a337628636a7994bf64a6a8c0fce7b839536511
manseo00 <84060692+manseo00@users.noreply.github.com>
2023-10-20 20:55:44 +0900

    Update 간단한 식 계산하기.java

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -6,7 +6,7 @@ class Solution {
         
         int num1 = Integer.parseInt(arr[0]);
         int num2 = Integer.parseInt(arr[2]);
-        
+         
         if(arr[1].equals("+")){
             answer = num1+num2;
         }else if(arr[1].equals("-")){
